c1ad4c2528a3c7807db9267a26e076220745c22a
alextyang <koopakondra@gmail.com>
2025-08-21 12:06:16 -0400

    Refactor caching mechanism to support multiple referrers and update cache file naming

--- app/v0/[[...path]]/route.ts
+++ app/v0/[[...path]]/route.ts
@@ -2,12 +2,17 @@ import { NextRequest } from "next/server";
 
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 const REFRESH_INTERVAL = 5 * 60 * 1000; // 5 minutes
+const FORGET_INTERVAL = 24 * 60 * 60 * 1000; // 24 hours
 
-let cache: { [key: string]: any } = {};
-let timestamps: { [key: string]: number } = {};
+let cache: { [referrer: string]: { [url: string]: any } } = {};
+let timestamps: { [referrer: string]: { [url: string]: number } } = {};
 
 export async function GET(request: NextRequest) {
     let { searchParams, pathname } = request.nextUrl;
+
+    let pageKey = request.headers.get("referer");
+    const referrer = pageKey?.split("/")[2] || "unknown";
+
     // Extract path segments from the pathname
     let path = pathname
         .replace(/^\/app\/|\/$/g, "") // Remove leading '/app/' and trailing '/'
@@ -25,13 +30,18 @@ export async function GET(request: NextRequest) {
 
     let data: any;
 
-    if (cache[url]) {
-        console.log("\n[API] Cache hit for URL:", decodeURIComponent(url));
-        data = cache[url];
+    if (!cache[referrer]) {
+        cache[referrer] = {};
+        timestamps[referrer] = {};
+    }
 
-        const lastUpdated = timestamps[url] || 0;
+    if (cache[referrer][url]) {
+        console.log("\n[API] Cache hit for URL:", decodeURIComponent(url));
+        data = cache[referrer][url];
+
+        const lastUpdated = timestamps[referrer][url] || 0;
         if (Date.now() - lastUpdated > REFRESH_INTERVAL)
-            refreshCache(url);
+            refreshCache(url, referrer);
 
         return new Response(JSON.stringify(data), {
             status: 200,
@@ -53,11 +63,11 @@ export async function GET(request: NextRequest) {
     data = await response.json();
 
     if (response.ok) {
-        cache[url] = data;
-        timestamps[url] = Date.now();
+        cache[referrer][url] = data;
+        timestamps[referrer][url] = Date.now();
     }
 
-    saveCache();
+    saveCache(referrer);
 
     console.log("[API] Response: ", response.status);
 
@@ -71,19 +81,19 @@ export async function GET(request: NextRequest) {
     );
 }
 
-async function saveCache() {
-    const cacheString = `export const cache = ${JSON.stringify(cache)};
+async function saveCache(referrerHostname: string) {
+    const cacheString = `export const cache = ${JSON.stringify(cache[referrerHostname])};
     window.airtableCache = cache;`;
 
     const fs = require('fs');
     const path = require('path');
-    const cacheFilePath = path.join(process.cwd(), 'public', 'cache.js');
+    const cacheFilePath = path.join(process.cwd(), 'public', 'cache-' + referrerHostname + '.js');
 
     fs.writeFileSync(cacheFilePath, cacheString, 'utf8');
     console.log("[API] Cache saved to", cacheFilePath);
 }
 
-async function refreshCache(url: string) {
+async function refreshCache(url: string, referrerHostname: string) {
     const response = await fetch(url, {
         headers: {
             Authorization: `Bearer ${AIRTABLE_API_KEY}`,
@@ -94,12 +104,20 @@ async function refreshCache(url: string) {
     const data = await response.json();
 
     if (response.ok) {
-        cache[url] = data;
-        timestamps[url] = Date.now();
+        cache[referrerHostname][url] = data;
+        timestamps[referrerHostname][url] = Date.now();
         console.log("[API] Cache refreshed for URL:", decodeURIComponent(url));
     }
 
-    saveCache();
+    for (const key in cache[referrerHostname]) {
+        if (Date.now() - timestamps[referrerHostname][key] > FORGET_INTERVAL) {
+            console.log("[API] Forgetting cache for URL:", decodeURIComponent(key));
+            delete cache[key];
+            delete timestamps[key];
+        }
+    }
+
+    saveCache(referrerHostname);
 }
 
 // http://localhost:4444/v0/appHcZTzlfXAJpL7I/tblm2TqCcDcx94nA2?filterByFormula=OR(FIND('September 2025', ARRAYJOIN({Cohort}, ',')) > 0, {Cohort} = 'September 2025',FIND('October 2025', ARRAYJOIN({Cohort}, ',')) > 0, {Cohort} = 'October 2025',FIND('November 2025', ARRAYJOIN({Cohort}, ',')) > 0, {Cohort} = 'November 2025')
